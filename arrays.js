d76ed14e4684691a4e9e2ea011e3b22f5d7bd4cc
Ricky Adams <mdaylon98@gmail.com>
2022-08-31 15:39:06 -0400

    Array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var fruit = ["apples", "oranges", "pears", "plums",]
 var ages = [27, 65, 87, 45]
 var studentDebt = [true, false, true, false]
+
+/* this will remove the last element from the fruit array */
+fruit.pop()
+console.log(fruit)
+// this will remove the first element from the ages array
+ages.shift
+console.log(ages)
+/* this will show me the 1st and 4th element in the studentDebt array */
+studentDebt.slice(1,4)
+console.log(studentDebt)
